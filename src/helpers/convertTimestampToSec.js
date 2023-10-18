f86c962fecaf9b4d8342197c44e953783ceaad4d
eddyionescu <eddyionescu@gmail.com>
2023-10-18 18:16:58 -0500

    Change imports

--- src/helpers/convertTimestampToSec.js
+++ src/helpers/convertTimestampToSec.js
@@ -1,5 +1,5 @@
 
-export default convertTimestampToSec = timestamp => {
+module.exports = timestamp => {
   if (timestamp > 2147483647) {
     // This is the epoch for January 1, 2038, after which 32-bit integers will no longer work for timestmaps.
     // Needed as some timestamps in files saved by Orion might be using milliseconds instead (the default for JS).
